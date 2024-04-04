5d6fa072ddbc880a87a712df82da067a49040e2d
Snigdha Gupta <123159026+Snigdha2121@users.noreply.github.com>
2024-04-04 23:41:47 +0530

    Translator.py

--- Translator.py
+++ Translator.py
@@ -1,9 +1,9 @@
 from googletrans import Translator
 
 msg = input("Enter your message: ")
-lng = input("Enter language you want to translate in: ")
+lng = input("Enter the language you want to translate into: ")
 
 translator = Translator()
 translated_text = translator.translate(msg, dest=lng)
 
-print(translated_text.text)
+print("The translated message: ",translated_text.text)
